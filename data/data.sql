192790ba1e08ecfbfa67472124c1f323a2958317
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-10-27 08:20:53 +0100

    Annotate the query

--- data/data.sql
+++ data/data.sql
@@ -3,18 +3,24 @@ data AS (
   SELECT
     id,
     date,
+    -- The data of the previous observation for the station and year
     LAG(date) OVER (station_year) AS date_last,
     latitude,
     longitude,
+    -- Convert from tenths of a unit to units
     value / 10 AS temperature
   FROM
     `bigquery-public-data.ghcn_d.ghcnd_201*`
   INNER JOIN
     `bigquery-public-data.ghcn_d.ghcnd_stations` USING (id)
   WHERE
+    -- Take years from 2010 to 2019
     CAST(_TABLE_SUFFIX AS INT64) BETWEEN 0 AND 9
+    -- Take months from June to August
     AND EXTRACT(MONTH FROM date) BETWEEN 6 AND 8
-    AND element = 'TMIN'
+    -- Take the average temperature
+    AND element = 'TAVG'
+    -- Take observations passed spatio-temporal quality-control checks
     AND qflag IS NULL
   WINDOW
     station_year AS (
@@ -27,11 +33,13 @@ SELECT
   MIN(date) AS date,
   latitude,
   longitude,
+  -- Compute gaps between observatios
   ARRAY_AGG(DATE_DIFF(date, IFNULL(date_last, date), DAY) ORDER BY date) AS step,
   ARRAY_AGG(temperature ORDER BY date) AS temperature
 FROM
   data
 GROUP BY
+  -- Group to have have one record per station and year
   id, latitude, longitude, EXTRACT(YEAR FROM date)
 ORDER BY
   id, date
