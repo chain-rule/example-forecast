098ebbc9a0d445eacc72d070d3c97bc1d71971de
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-10-27 08:14:41 +0100

    Update the query

--- data/data.sql
+++ data/data.sql
@@ -1,17 +1,37 @@
+WITH
+data AS (
+  SELECT
+    id,
+    date,
+    LAG(date) OVER (station_year) AS date_last,
+    latitude,
+    longitude,
+    value / 10 AS temperature
+  FROM
+    `bigquery-public-data.ghcn_d.ghcnd_201*`
+  INNER JOIN
+    `bigquery-public-data.ghcn_d.ghcnd_stations` USING (id)
+  WHERE
+    CAST(_TABLE_SUFFIX AS INT64) BETWEEN 0 AND 9
+    AND EXTRACT(MONTH FROM date) BETWEEN 6 AND 8
+    AND element = 'TMIN'
+    AND qflag IS NULL
+  WINDOW
+    station_year AS (
+      PARTITION BY id, EXTRACT(YEAR FROM date)
+      ORDER BY date
+    )
+)
 SELECT
   id,
+  MIN(date) AS date,
   latitude,
   longitude,
-  ARRAY_AGG(date ORDER BY date) AS date,
-  ARRAY_AGG(value / 10 ORDER BY date) AS temperature
+  ARRAY_AGG(DATE_DIFF(date, IFNULL(date_last, date), DAY) ORDER BY date) AS step,
+  ARRAY_AGG(temperature ORDER BY date) AS temperature
 FROM
-  `bigquery-public-data.ghcn_d.ghcnd_*`
-INNER JOIN
-  `bigquery-public-data.ghcn_d.ghcnd_stations` USING (id)
-WHERE
-  REGEXP_CONTAINS(_TABLE_SUFFIX, '201[0-8]')
-  AND EXTRACT(MONTH FROM date) BETWEEN 6 AND 8
-  AND element = 'TMIN'
-  AND qflag IS NULL
+  data
 GROUP BY
-  1, 2, 3
+  id, latitude, longitude, EXTRACT(YEAR FROM date)
+ORDER BY
+  id, date
