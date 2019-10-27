c6b57e2135918de9aa3350e983a8927043d4a731
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-10-27 17:50:01 +0000

    Add timestamps

--- forecast/main.py
+++ forecast/main.py
@@ -1,7 +1,17 @@
 import argparse
+import datetime
 import json
 
 from forecast.task import Task
+
+
+def _format(object, now):
+    if isinstance(object, dict):
+        return {name: _format(value, now) for name, value in object.items()}
+    if isinstance(object, str):
+        return now.strftime(object)
+    return object
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,4 +19,5 @@ if __name__ == '__main__':
     parser.add_argument('--config', type=str, required=True)
     arguments = parser.parse_args()
     with open(arguments.config) as file:
-        Task().run(arguments.action, json.load(file))
+        config = _format(json.load(file), datetime.datetime.now())
+        Task().run(arguments.action, config)
